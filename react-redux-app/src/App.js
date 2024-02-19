827f41e0976fce6bd2aa59b58942e7351d5b46df
Raghav Kattel <arg.ahv7@gmail.com>
2024-02-19 20:22:34 +0545

    Add initial files and configurations for React Redux app

---
+++ react-redux-app/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./slices/counterSlice";
+
+function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
